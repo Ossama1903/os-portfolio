5aee6c9a51c74d4fda87924f6dfcef0b2a7728fe
Ossama Bilal <sheikhusamabilal@gmail.com>
2024-06-17 19:28:52 +0500

    cls and clear commands working in terminal

--- src/app/components/Terminal/index.tsx
+++ src/app/components/Terminal/index.tsx
@@ -1,10 +1,17 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
 
+interface Command {
+  command: string;
+  directory: string;
+}
+
 export default function Terminal() {
   const [inputValue, setInputValue] = useState("");
   const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const [commandHistory, setCommandHistory] = useState<string[]>([]);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [commandHistory, setCommandHistory] = useState<Command[]>([]);
+  const [directoryPath, setDirectoryPath] = useState("user@macbook ~ %");
 
   useEffect(() => {
     if (textareaRef.current) {
@@ -13,6 +20,12 @@ export default function Terminal() {
     }
   }, [inputValue]);
 
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }, [commandHistory]);
+
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue(e.target.value);
   };
@@ -20,13 +33,22 @@ export default function Terminal() {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
-      // Execute your function here
       executeCommand();
     }
   };
 
   const executeCommand = () => {
-    setCommandHistory([...commandHistory, inputValue]);
+    if (
+      inputValue.toLowerCase() === "cls" ||
+      inputValue.toLowerCase() == "clear"
+    ) {
+      setCommandHistory([]);
+    } else {
+      setCommandHistory([
+        ...commandHistory,
+        { directory: directoryPath, command: inputValue },
+      ]);
+    }
     setInputValue("");
   };
 
@@ -39,12 +61,15 @@ export default function Terminal() {
           <div className="w-3 h-3 bg-green-500 rounded-[50%] mx-1" />
         </div>
       </div>
-      <div className="h-full bg-gray-800 p-2 rounded-b-lg overflow-auto">
+      <div
+        ref={scrollRef}
+        className="h-full bg-gray-800 p-2 rounded-b-lg overflow-auto custom-scrollbar"
+      >
         {commandHistory.map((command, index) => (
           <div key={index} className="text-white flex flex-wrap">
-            <span>user@macbook ~ %</span>
+            <span>{command.directory}</span>
             <textarea
-              defaultValue={command.trim()}
+              defaultValue={command.command.trim()}
               readOnly
               className="flex-1 text-yellow-200 bg-transparent border-none focus:outline-none resize-none ml-2 overflow-hidden"
               style={{ minHeight: "1.5rem" }}
@@ -52,7 +77,7 @@ export default function Terminal() {
           </div>
         ))}
         <div className="text-white flex flex-wrap">
-          <span>user@macbook ~ %</span>
+          <span>{directoryPath}</span>
           <textarea
             ref={textareaRef}
             value={inputValue}
